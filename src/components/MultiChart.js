272e309a7c52a284dc3f9ef2e42eb4d0349b21a4
DarekR88 <dradmclad@gmail.com>
2019-11-24 23:54:06 -0600

    code cleaned up and finalized

--- src/components/MultiChart.js
+++ src/components/MultiChart.js
@@ -1,109 +1,91 @@
 import React from 'react';
-import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
-import { useSelector } from "react-redux";
-import MultipleMetrics from "../Features/MultipleMetrics/multipleMetrics";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { useSelector } from 'react-redux';
+import MultipleMetrics from '../Features/MultipleMetrics/multipleMetrics';
 import Card from '../components/Card';
 
 export default function MultiChart() {
+  const multiData = useSelector(state => state.multipleData.multipleData);
 
-    const multiData = useSelector(state => state.multipleData.multipleData)
+  // function for converting camel case to sentence case
+  const convertCase = metric => {
+    switch (metric) {
+      case 'injValveOpen':
+        return 'INJ Valve Open';
 
-    return (
-        <div>
-            <MultipleMetrics />
-            {multiData.map((i) => {
-                let metricName = ''
-                const currentData = i.measurements[i.measurements.length - 1]
-                switch (i.metric) {
-                    case 'injValveOpen':
-                        metricName = 'INJ Valve Open'
-                        break;
-                    case 'oilTemp':
-                        metricName = 'Oil Temp'
-                        break;
-                    case 'tubingPressure':
-                        metricName = 'Tubing Pressure'
-                        break;
-                    case 'flareTemp':
-                        metricName = 'Flare Temp'
-                        break;
-                    case 'casingPressure':
-                        metricName = 'Casing Pressure'
-                        break;
-                    case 'waterTemp':
-                        metricName = 'Water Temp'
-                        break;
-                    default:
-                        metricName = 'metric'
-                }
+      case 'oilTemp':
+        return 'Oil Temp';
 
-                return (<Card metric={metricName} data={currentData.value} />)
-            })}
-            <LineChart width={1000} height={600}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="at" type="category" allowDuplicatedCategory={false} />
-                <YAxis dataKey="value" />
-                <Tooltip />
-                <Legend layout="vertical" verticalAlign="middle" align="right" />
-                {multiData.map((i) => {
-                    let color = ''
-                    let metricName = ''
-                    switch (i.metric) {
-                        case 'injValveOpen':
-                            color = '#1BD82A'
-                            break;
-                        case 'oilTemp':
-                            color = '#000000'
-                            break;
-                        case 'tubingPressure':
-                            color = '#FF0000'
-                            break;
-                        case 'flareTemp':
-                            color = '#FFB201'
-                            break;
-                        case 'casingPressure':
-                            color = '#830BEE'
-                            break;
-                        case 'waterTemp':
-                            color = '#000CFF'
-                            break;
-                        default:
-                            color = '#00FFE0'
-                    }
+      case 'tubingPressure':
+        return 'Tubing Pressure';
 
-                    switch (i.metric) {
-                        case 'injValveOpen':
-                            metricName = 'INJ Valve Open'
-                            break;
-                        case 'oilTemp':
-                            metricName = 'Oil Temp'
-                            break;
-                        case 'tubingPressure':
-                            metricName = 'Tubing Pressure'
-                            break;
-                        case 'flareTemp':
-                            metricName = 'Flare Temp'
-                            break;
-                        case 'casingPressure':
-                            metricName = 'Casing Pressure'
-                            break;
-                        case 'waterTemp':
-                            metricName = 'Water Temp'
-                            break;
-                        default:
-                            metricName = 'metric'
-                    }
-    
-                    return (<Line dataKey="value" data={i.measurements} name={metricName} key={i.metric} dot={false} stroke={color} />)
-                })}
-            </LineChart>
-        </div>
+      case 'flareTemp':
+        return 'Flare Temp';
 
-    );
+      case 'casingPressure':
+        return 'Casing Pressure';
 
+      case 'waterTemp':
+        return 'Water Temp';
 
+      default:
+        return 'metric';
+    }
+  };
+
+  // function for changing the color of the lines of the metrics based on name
+  const metricColor = metric => {
+    switch (metric) {
+      case 'injValveOpen':
+        return '#1BD82A';
+
+      case 'oilTemp':
+        return '#000000';
+
+      case 'tubingPressure':
+        return '#FF0000';
+
+      case 'flareTemp':
+        return '#FFB201';
+
+      case 'casingPressure':
+        return '#830BEE';
+
+      case 'waterTemp':
+        return '#000CFF';
+
+      default:
+        return '#00FFE0';
+    }
+  };
+
+  return (
+    <>
+      <MultipleMetrics />
+      {multiData.map(i => {
+        const currentData = i.measurements[i.measurements.length - 1];
+
+        return <Card metric={convertCase(i.metric)} data={currentData.value} />;
+      })}
+      <LineChart width={1000} height={600}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="at" type="category" allowDuplicatedCategory={false} />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend layout="vertical" verticalAlign="middle" align="right" />
+        {multiData.map(i => {
+          return (
+            <Line
+              dataKey="value"
+              data={i.measurements}
+              name={convertCase(i.metric)}
+              key={i.metric}
+              dot={false}
+              stroke={metricColor(i.metric)}
+            />
+          );
+        })}
+      </LineChart>
+    </>
+  );
 }
-
-
